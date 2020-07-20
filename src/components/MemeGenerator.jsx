6c5aa7052ee5f38d67c230da6d2fe6f9e0aab06e
ryguy806 <L1ghtSab3r11!!>
2020-07-20 13:23:03 -0700

    initial commit. Created files

---
+++ src/components/MemeGenerator.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class MemeGenerator extends Component {
+
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>MEME GENREATOR</h1>
+            </div>
+        )
+    }
+}
+
+export default MemeGenerator;
